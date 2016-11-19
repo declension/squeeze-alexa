83024a537a39e0c598be922ee74b790371ac3530
Nick B <nick@declension.net>
2016-11-19 12:42:54 +0000

    Support custom loop / shuffle & tidy
     * Support the custom intents for looping and shuffling, in addition to Amazon ones.
     * Improve now playing response
     * Extract generic interface elsewhere
     * Remove old docstrings

---
+++ squeezealexa/alexa/handlers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Nick Boultbee
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+
+
+class AlexaHandler(object):
+
+    def on_session_ended(self, session_ended_request, session):
+        """ Called when the user ends the session.
+        Is not called when the skill returns should_end_session=true
+        """
+        pass
+
+    def on_session_started(self, request, session):
+        """Called when the session starts """
+        pass
+
+    def on_launch(self, launch_request, session):
+        """Called when the user launches the skill
+        without specifying what they want"""
+
+    def on_intent(self, intent_request, session):
+        """Called when the user specifies an intent for this skill"""
+        pass
--- squeezealexa/main.py
+++ squeezealexa/main.py
@@ -6,43 +6,17 @@
 # published by the Free Software Foundation
 
 
-"""
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
-
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
-"""
-
 from __future__ import print_function
 
-from squeezealexa.alexa.intents import Audio, General, Custom, Power
+from squeezealexa.alexa.handlers import AlexaHandler
+from squeezealexa.alexa.intents import Audio, General, Custom, Power, \
+    CustomAudio
 from squeezealexa.alexa.response \
     import speechlet_fragment, build_response, build_audio_response
 from squeezealexa.settings import *
 from squeezealexa.squeezebox.server import Server
 
 print_d = print_w = print
-
-
-class AlexaHandler(object):
-    def on_session_ended(self, session_ended_request, session):
-        """ Called when the user ends the session.
-        Is not called when the skill returns should_end_session=true
-        """
-        pass
-
-    def on_session_started(self, request, session):
-        """Called when the session starts """
-        pass
-
-    def on_launch(self, launch_request, session):
-        """Called when the user launches the skill
-        without specifying what they want"""
-
-    def on_intent(self, intent_request, session):
-        """Called when the user specifies an intent for this skill"""
-        pass
 
 
 class SqueezeAlexa(AlexaHandler):
@@ -58,18 +32,11 @@ class SqueezeAlexa(AlexaHandler):
 
         print_d("Entering interactive mode for sessionId=%s"
                 % session['sessionId'])
-        return self.get_welcome_response()
-
-    def get_welcome_response(self):
-        """ If we wanted to initialize the session to have some attributes
-        we could add those here
-        """
-        card_title = "Welcome"
         speech_output = "Squeezebox is online. Please try some commands."
         reprompt_text = "Try resume, pause, next, previous " \
                         "or ask Squeezebox to turn it up or down"
         return build_response(speechlet_fragment(
-            card_title, speech_output, reprompt_text))
+            "Welcome", speech_output, reprompt_text))
 
     @classmethod
     def get_server(cls):
@@ -83,6 +50,8 @@ class SqueezeAlexa(AlexaHandler):
                                  debug=True,
                                  ca_file=CA_FILE_PATH,
                                  cert_file=CERT_FILE_PATH)
+        else:
+            print_d("Reusing cached %r" % cls._server)
         return cls._server
 
     def on_intent(self, intent_request, session):
@@ -113,7 +82,7 @@ class SqueezeAlexa(AlexaHandler):
             details = self.get_server().get_track_details()
             title = details['current_title']
             artist = details['artist']
-            desc = "Currently playing \"%s\" by %s" % (title, artist)
+            desc = "Currently playing: \"%s\", by %s" % (title, artist)
             heading = "Now playing: \"%s\"" % title
             return build_response(speechlet_fragment(heading, desc))
 
@@ -127,19 +96,19 @@ class SqueezeAlexa(AlexaHandler):
             return build_response(
                 speechlet_fragment("Decrease Volume", "OK, it's quieter now."))
 
-        elif intent_name == Audio.SHUFFLE_ON:
+        elif intent_name in [Audio.SHUFFLE_ON, CustomAudio.SHUFFLE_ON]:
             self.get_server().set_shuffle(True)
             return build_audio_response("Shuffle on")
 
-        elif intent_name == Audio.SHUFFLE_OFF:
+        elif intent_name in [Audio.SHUFFLE_OFF, CustomAudio.SHUFFLE_OFF]:
             self.get_server().set_shuffle(False)
             return build_audio_response("Shuffle off")
 
-        elif intent_name == Audio.LOOP_ON:
+        elif intent_name in [Audio.LOOP_ON, CustomAudio.LOOP_ON]:
             self.get_server().set_repeat(True)
             return build_audio_response("Repeat on")
 
-        elif intent_name == Audio.LOOP_OFF:
+        elif intent_name in [Audio.LOOP_OFF, CustomAudio.LOOP_OFF]:
             self.get_server().set_repeat(False)
             return build_audio_response("Repeat off")
 
@@ -154,7 +123,7 @@ class SqueezeAlexa(AlexaHandler):
                 speechlet_fragment("Players all on", "Ready."))
 
         elif intent_name == General.HELP:
-            return self.get_welcome_response()
+            return self.on_launch(intent_request, session)
 
         elif intent_name in [General.CANCEL, General.STOP]:
             return self.on_session_ended(intent_request, session)
