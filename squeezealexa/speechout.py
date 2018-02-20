c20b0930682f8e59e57334d36d1428393458610d
dfranzen <dfranzen@github.com>
2018-02-20 16:40:39 +0200

    + Factored output strings into speechout.py + added response creators for multilingual output + added rebust substitution for outputs * matched speech_response and audio_response parameters + added German utterances * removed temporary files from create_zip

---
+++ squeezealexa/speechout.py
@@ -0,0 +1,80 @@
+speechoutput_en = {
+    'launch' : "Squeezebox is online. Please try some commands.",
+    'session_ended': "Hasta la vista. Baby.",
+    'unknown_intent': "Sorry, I don't know how to process \"{0}\"",
+    'previous': "Rewind!",
+    'next': "Yep, pretty lame.",
+    'current': "Currently playing: \"{0}\"" ,
+    'current_by': ",by {0}",
+    'current_none': "Nothing playing",
+    'inc_vol': "Pumped it up",
+    'dec_vol': "OK, quieter now.",
+    'select_player': "Selected {1}",
+    'select_player_nf': "I only found these players:{0}. Could you try again?",
+    'shuffle_on': "Shuffle is now on",
+    'shuffle_off': "Shuffle is now off",
+    'loop_on': "Repeat is now on",
+    'loop_off': "Repeat is now off",
+    'player_on': "{0} is now on",
+    'player_on_select': ", and is selected.",
+    'player_off': "{0} is now off",
+    'all_on': "Ready to rock",
+    'all_off': "Silence",
+    'play_random_mix_nf': "Can't find genres: {0}"
+}
+
+titleoutput_en = {
+    'launch': "Welcome",
+    'session_ended': "Session Ended",
+    'select_player': "Selected player {0}",
+    'select_player_nf': "No player called \"{0}\"",
+    'select_player_nk': "Didn't recognise a player name",
+    'player_on' : "Switched {0} on",
+    'player_off' : "Switched {0} off"
+}
+
+textoutput_en = {
+    'unknown_intent': "Unknown intent: \"{0}\"",
+    'current': "Now playing: \"{0}\"",
+    'inc_vol': "Increase Volume",
+    'dec_vol': "Decrease Volume",
+    'shuffle_on' : "Shuffle on",
+    'shuffle_off' : "Shuffle off",
+    'loop_on' : "Repeat on",
+    'loop_off' : "Repeat off",
+    'player_on' : "Switched {0} on",
+    'player_off' : "Switched {1} off",
+    'all_off': "Players all off",
+    'all_on': "All On.",
+    'play_playlist_nh_none': "There are no playlists",
+    'play_playlist_nh': "Didn't hear a playlist there. "
+                        "You could try the \"{0}\" playlist?",
+    'play_playlist_none': "No Squeezebox playlists found",
+    'play_playlist' : "Playing \"{0}\" playlist",
+    'play_playlist_nf' : "Couldn't find a playlist matching \"{0}\"."
+                     "How about the \"{1}\" playlist?",
+    'play_random_mix': "Playing mix of {0}",
+    'play_random_mix_nf': "Don't understand requested genres {0}"
+}
+
+repromptoutput_en = {
+    'launch': "Try resume, pause, next, previous " \
+                        "or ask Squeezebox to turn it up or down"
+}
+
+
+speechoutput = {
+    'EN' : speechoutput_en
+}
+
+titleoutput = {
+    'EN' : titleoutput_en
+}
+
+textoutput = {
+    'EN' : textoutput_en
+}
+
+repromptoutput = {
+    'EN': repromptoutput_en
+}
