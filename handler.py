a3cfda1bad73b6a92ff5d420fb4c594c91128967
Nick B <nick@declension.net>
2018-04-14 19:14:00 +0100

    Missed a file from previous commit...

--- handler.py
+++ handler.py
@@ -10,8 +10,19 @@
 #
 #   See LICENSE for full license
 
+from traceback import format_exc
+
+from squeezealexa import settings
+from squeezealexa.alexa.response import speech_response
 from squeezealexa.main import SqueezeAlexa
 from squeezealexa.settings import APPLICATION_ID
+from squeezealexa.utils import print_w
+
+try:
+    from squeezealexa.i18n import _
+except ImportError:
+    def _(s):
+        return s
 
 
 def lambda_handler(event, context):
@@ -19,4 +30,14 @@ def lambda_handler(event, context):
     etc.) The JSON body of the request is provided in the event parameter.
     """
     sqa = SqueezeAlexa(app_id=APPLICATION_ID)
-    return sqa.handle(event, context)
+    try:
+        return sqa.handle(event, context)
+    except Exception as e:
+        if not settings.USE_SPOKEN_ERRORS:
+            raise e
+        # Work with AWS stack-trace log magic
+        print_w(format_exc().replace('\n', '\r'))
+        error = str(e.msg if hasattr(e, "msg") else e)
+        return speech_response(title=_("All went wrong"),
+                               text=_("Oh dear: {type}. {message}").format(
+                                   type=type(e).__name__, message=error))
