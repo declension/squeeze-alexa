fc291d763cdf2e0fadcd2c538ee1d69a5e45e4ed
Nick B <nick@declension.net>
2018-11-20 12:37:02 +0000

    Build:
     * Release should always run the build, too dangerous otherwise
     * Tidy up some more directory stuff

--- bin/release.sh
+++ bin/release.sh
@@ -14,6 +14,8 @@ echo "Checking for uncommitted changes..."
 files=$(git diff --cached --exit-code --name-only) || die "You have staged Git changes. Commit or stash: \n $files"
 files=$(git diff --exit-code --name-only) || die "You have unstaged Git changes. Commit or stash: \n $files"
 
+echo "Running the local build..."
+$root/bin/build.sh
 pushd "$root/dist" >/dev/null || die "Perhaps you haven't run the build yet?"
 version=${1:-latest}
 echo "<<<< Doing release build for version '$version'. Continue?... >>>>"
@@ -21,7 +23,7 @@ if [ "$1" != "-y" ]; then
     read -n 1 -p "Continue? (ctrl-c to abort)"
 fi
 
-$(dirname $0)/compile-translations
+$root/bin/compile-translations
 
 echo -e "\nContinuing with build...\n"
 output="squeeze-alexa-release-$version.zip"
@@ -42,7 +44,7 @@ RELEASE_EXCLUDES=$(tr '\n' ' ' <<< """
 test-results
 bin/build.sh""")
 
-echo "Creating ZIP (excluding $RELEASE_EXCLUDES)"
+echo "Creating $output (excluding $RELEASE_EXCLUDES)"
 rm "$release_dir/$output" 2>/dev/null || true
 zip -r "$release_dir/$output" * -x $RELEASE_EXCLUDES
 cd "$root"
